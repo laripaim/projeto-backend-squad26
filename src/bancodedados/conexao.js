8bc1119104f5bb019d7f8051528d620451a66b8c
Larissa Paim <larissa.paim@gmail.com>
2022-04-10 21:48:35 -0300

    Última tentativa do dia para resolver o erro da aplicação

--- src/bancodedados/conexao.js
+++ src/bancodedados/conexao.js
@@ -5,7 +5,7 @@ const knex = require("knex")({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        port: 5432,
+        port: process.env.PORT,
         ssl: {
             rejectUnauthorized: false
         }
